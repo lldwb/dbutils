ab27b20d6e7ee2ee913af9da4a8ac4eb9561883d
安然的尾巴 <3247187440@qq.com>
2023-09-05 20:32:26 +0800

    完善

--- src/main/java/top/lldwb/db/util/handle/AbstractListHandler.java
+++ src/main/java/top/lldwb/db/util/handle/AbstractListHandler.java
@@ -9,7 +9,9 @@ import java.util.List;
 
 /**
  * 实现ResultSetHandler<List<T>>,这样handler返回类型为List
- * 同时handler负责接收数据,创建一个抽象方法handleRow负责让子类重写并调用子类对应的单行处理器
+ * 同时handler负责接收数据
+ * 创建一个抽象方法handleRow负责让子类重写并调用子类对应的单行处理器
+ *
  * @param <T>
  */
 public abstract class AbstractListHandler<T> implements ResultSetHandler<List<T>> {
